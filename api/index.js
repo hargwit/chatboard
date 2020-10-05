95743494e11a5e9b484cfa827e1e2a3ca3565d7c
Harry Withers <harryw@mohc.net>
2020-10-05 06:47:31 +0000

    Chore: init api project

---
+++ api/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+function startServer() {
+    const app = express()
+
+    app.listen(3001, () => {
+        console.log(`
+            ###############################################
+                   Server listening on port 3001
+            ###############################################
+
+        `)
+    })
+}
+
+startServer()
