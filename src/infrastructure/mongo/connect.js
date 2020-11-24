7f638beca609d6db84784904e2c5c3362e8c6d1f
Harry Withers <harryw@mohc.net>
2020-11-24 08:12:29 +0000

    refactor: move console log in database connector

--- src/infrastructure/mongo/connect.js
+++ src/infrastructure/mongo/connect.js
@@ -2,9 +2,8 @@ import * as inMem from './inmem'
 import * as mongo from './mongo'
 
 export const connect = async ({ uri, useInMem = false }) => {
-    console.log(useInMem)
-
     if (useInMem) {
+        console.log('Using in memory database')
         await inMem.connect()
     } else {
         await mongo.connect(uri)
